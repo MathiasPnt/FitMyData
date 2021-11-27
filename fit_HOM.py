e65985bdbfaaea104f92b9cdc298b91f8a6e7c67
MathiasPnt <54806615+MathiasPnt@users.noreply.github.com>
2021-11-27 18:50:13 +0100

    Correct a few problems

--- fit_HOM.py
+++ fit_HOM.py
@@ -55,55 +55,55 @@ def find_sidepeaks(data):
 def get_vhom(data, peak_width, peak_sep, central_peak, num_peaks):
     return get_HOM_1input(data, peak_width, peak_sep, central_peak, num_peaks)
 
-    # Uploading data (in .txt or .dat format only). You can also drag and drop.
-    file = st.file_uploader('Load data', type={"txt", "dat"}, help='Upload your data here')
+# Uploading data (in .txt or .dat format only). You can also drag and drop.
+file = st.file_uploader('Load data', type={"txt", "dat"}, help='Upload your data here')
 
-    col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4 = st.columns(4)
 
-    if file is not None:
-        # Select which TimeTagger is used
-        with col1:
-            timetagger = st.radio("Select correlator", ('Swabian', 'HydraHarp', 'Custom dataset'))
-        # Get the histogram from the data file depending on which correlator was used.
-        if timetagger == 'Swabian':
-            # The data has been saved using:
-            # np.savetxt(file.txt, [index, hist])
-            # we only use the hist
-            data = np.loadtxt(file)[1]
-        if timetagger == 'HydraHarp':
-            with col2:
-                # Channel used with the HydraHarp (starts at 0).
-                use_channel = st.number_input('Use channel:', value=0)
-            # For file extracted in ASCII from Picoquant software there are 10 lines of information that we skip.
-            data = np.loadtxt(file, skiprows=10)[:, use_channel]
-        if timetagger == 'Custom dataset':
-            with col2:
-                structure_data = st.radio("Data is stored in:", ('List', 'Lines', 'Columns'))
-            if structure_data == 'List':
-                data = np.loadtxt(file)
-            if structure_data == 'Lines':
-                with col3:
-                    use_line = st.number_input('Use line:', value=0)
-                data = np.loadtxt(file)[use_line]
-            if structure_data == 'Columns':
-                with col3:
-                    use_col = st.number_input('Use column:', value=0)
-                with col4:
-                    skip = st.number_input('Skip rows:', value=0)
-                data = np.loadtxt(file, skiprows=skip)[:, use_col]
+if file is not None:
+    # Select which TimeTagger is used
+    with col1:
+        timetagger = st.radio("Select correlator", ('Swabian', 'HydraHarp', 'Custom dataset'))
+    # Get the histogram from the data file depending on which correlator was used.
+    if timetagger == 'Swabian':
+        # The data has been saved using:
+        # np.savetxt(file.txt, [index, hist])
+        # we only use the hist
+        data = np.loadtxt(file)[1]
+    if timetagger == 'HydraHarp':
+        with col2:
+            # Channel used with the HydraHarp (starts at 0).
+            use_channel = st.number_input('Use channel:', value=0)
+        # For file extracted in ASCII from Picoquant software there are 10 lines of information that we skip.
+        data = np.loadtxt(file, skiprows=10)[:, use_channel]
+    if timetagger == 'Custom dataset':
+        with col2:
+            structure_data = st.radio("Data is stored in:", ('List', 'Lines', 'Columns'))
+        if structure_data == 'List':
+            data = np.loadtxt(file)
+        if structure_data == 'Lines':
+            with col3:
+                use_line = st.number_input('Use line:', value=0)
+            data = np.loadtxt(file)[use_line]
+        if structure_data == 'Columns':
+            with col3:
+                use_col = st.number_input('Use column:', value=0)
+            with col4:
+                skip = st.number_input('Skip rows:', value=0)
+            data = np.loadtxt(file, skiprows=skip)[:, use_col]
 
-        # Create time axis to plot
-        time = (np.arange(0, len(data)))
+    # Create time axis to plot
+    time = (np.arange(0, len(data)))
 
-        peaks, data_pk, ct_peak, pk_sep, pk_width = find_sidepeaks(data)
+    peaks, data_pk, ct_peak, pk_sep, pk_width = find_sidepeaks(data)
 
-        # Creating widget for the app. Here we put them in a sidebar.
-        # Position of the time stamp of the central peak (zero delay) of the histogram
-        # Number of side peaks used to normalise the central peak and get the g2
-        num_peaks = st.sidebar.slider('Number of peaks used for integration', 0, 20, 6)
-        central_peak = st.sidebar.number_input('Central peak', 0, len(data), ct_peak)
-        peak_width = st.sidebar.number_input('Peak width', 0, pk_sep, pk_width)
-        peak_sep = st.sidebar.number_input('Peak separation', 0, len(data), pk_sep)
+    # Creating widget for the app. Here we put them in a sidebar.
+    # Position of the time stamp of the central peak (zero delay) of the histogram
+    # Number of side peaks used to normalise the central peak and get the g2
+    num_peaks = st.sidebar.slider('Number of peaks used for integration', 0, 20, 6)
+    central_peak = st.sidebar.number_input('Central peak', 0, len(data), ct_peak)
+    peak_width = st.sidebar.number_input('Peak width', 0, pk_sep, pk_width)
+    peak_sep = st.sidebar.number_input('Peak separation', 0, len(data), pk_sep)
 
     HOM, errHOM = get_vhom(data, peak_width, peak_sep, central_peak, num_peaks)
 
