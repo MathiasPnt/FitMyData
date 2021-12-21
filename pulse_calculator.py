7b8b94b6e00aa98d0a087101d8f20f3fb0c3b5a9
MathiasPnt <54806615+MathiasPnt@users.noreply.github.com>
2021-12-21 14:48:43 +0100

    Add lorentzian pulse to pulse calculator

--- pulse_calculator.py
+++ pulse_calculator.py
@@ -34,6 +34,9 @@ def convert_to_s(value, unit):
 # Add a white space
 st.markdown('#')
 
+st.error("Computes the transform-limited, i.e. minimum possible pulse duration.")
+
+st.markdown('#')
 
 def get_calib():
     spectrometer = st.sidebar.radio("Select spectrometer",
@@ -52,7 +55,7 @@ def get_calib():
 
 # Get pulse duration from width (transform-limited)
 col1, col2 = st.columns([2, 1])
-FWHM = col1.number_input('Spectral width (FWHM)', value=1.000)
+FWHM = col1.number_input('Spectral width (FWHM)', value=1.0000)
 unit_FWHM = col2.selectbox('Unit', ("nm", "pm", "px"), key = 'unit FWHM')
 if unit_FWHM == "px":
     calib = get_calib()
@@ -61,18 +64,26 @@ col2, col3 = st.columns([2, 1])
 xc = col2.number_input('Center wavelength', value=925.0, key = 'xc FWHM to tau')
 unit_xc = col3.selectbox('Unit', ("nm", ), key = 'unit xc')
 
-# We define TBP_gauss = ∆nu*∆tau
 delta_nu = c0 * convert_to_m(FWHM, unit_FWHM) / convert_to_m(xc, unit_xc) ** 2
+# We define TBP_gauss = ∆nu*∆tau
 TBP_gauss = 2 * np.log(2) / np.pi
-delta_tau_output = TBP_gauss / delta_nu
+delta_tau_output_gauss = TBP_gauss / delta_nu
 
-text = 'Pulse duration (Gaussian): '+str(round(delta_tau_output*1e12,2)) +" ps"
+# We define TBP_lorentz = ∆nu*∆tau
+TBP_lorentz = np.log(2) * np.sqrt(np.sqrt(2)-1) / np.pi
+delta_tau_output_lorentz = TBP_lorentz / delta_nu
+
+type_pulse1 = st.radio('Type of pulse', ("Gaussian", "Lorentzian"), key = 'type-1')
+
+if type_pulse1 == "Gaussian":
+    text = 'Pulse duration: '+str(round(delta_tau_output_gauss*1e12,2)) +" ps"
+if type_pulse1 == "Lorentzian":
+    text = 'Pulse duration: ' + str(round(delta_tau_output_lorentz * 1e12, 2)) + " ps"
 display = '<p style="font-family:sans-serif; color:seagreen; font-size: 32px;">'+text+'</p>'
 delta_tau = st.markdown(display, unsafe_allow_html=True)
 
 # Add a white space
 st.markdown('#')
-
 
 # Get pulse duration from width (transform-limited)
 col5, col6 = st.columns([2, 1])
@@ -83,20 +94,36 @@ col7, col8 = st.columns([2, 1])
 xc = col7.number_input('Center wavelength', value=925.0, key = 'xc tau to FWHM')
 unit_xc = col8.selectbox('Unit', ("nm", ), key = 'unit xc bis')
 
-# We define TBP_gauss = ∆nu*∆tau
-TBP_gauss = 2 * np.log(2) / np.pi
-delta_nu = TBP_gauss / convert_to_s(delta_tau_input, unit_delta_tau)
-delta_lambda = delta_nu * convert_to_m(xc, unit_xc) ** 2 / c0
+delta_nu_gauss = TBP_gauss / convert_to_s(delta_tau_input, unit_delta_tau)
+delta_nu_lorentz = TBP_lorentz / convert_to_s(delta_tau_input, unit_delta_tau)
+
+delta_lambda_gauss = delta_nu_gauss * convert_to_m(xc, unit_xc) ** 2 / c0
+delta_lambda_lorentz = delta_nu_lorentz * convert_to_m(xc, unit_xc) ** 2 / c0
+
+type_pulse2 = st.radio('Type of pulse', ("Gaussian", "Lorentzian"), key = 'type-2')
 
 col9, col10 = st.columns([3, 1])
-in_px = col10.checkbox('in pixel?')
-if in_px:
+unit_result = col10.selectbox('Unit', ("px", "nm", "pm"), key = 'unit-result')
+if unit_result=='px':
     if unit_FWHM == "px":
         pass
     else:
         calib = get_calib()
-    text = 'Pulse width: ' + str(round(calib * delta_lambda*1e9,1)) + " px"
-else:
-    text = 'Pulse width: '+str(round(delta_lambda*1e9,2)) +" nm"
+    if type_pulse2 == "Gaussian":
+        text = 'Pulse width: ' + str(round(calib * delta_lambda_gauss*1e9, 2)) + " px"
+    if type_pulse2 == "Lorentzian":
+        text = 'Pulse width: ' + str(round(calib * delta_lambda_lorentz * 1e9, 2)) + " px"
+
+if unit_result=='nm':
+    if type_pulse2 == "Gaussian":
+        text = 'Pulse width: '+str(round(delta_lambda_gauss*1e9, 3)) +" nm"
+    if type_pulse2 == "Lorentzian":
+        text = 'Pulse width: '+str(round(delta_lambda_lorentz*1e9, 3)) +" nm"
+
+if unit_result=='pm':
+    if type_pulse2 == "Gaussian":
+        text = 'Pulse width: '+str(round(delta_lambda_gauss*1e12, 3)) +" pm"
+    if type_pulse2 == "Lorentzian":
+        text = 'Pulse width: '+str(round(delta_lambda_lorentz*1e12, 3)) +" pm"
 display = '<p style="font-family:sans-serif; color:seagreen; font-size: 32px;">'+text+'</p>'
 delta_lambda = col9.markdown(display, unsafe_allow_html=True)
