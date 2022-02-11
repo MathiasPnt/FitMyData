Changed g2 and HOM uncertainty calculation to poissonian simulation method. Corrected a few harmless typos  in app.py Added f-strings for g2 and HOM titles.

antibunching_toolbox.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-@Author: Mathias Pont
+@Authors: Mathias Pont
+@Contributors: Andreas Fyrillas
 """
 
 import numpy as np
@@ -92,9 +93,8 @@ def get_g2_1input(dat_g2, peak_width, peak_sep, central_peak, num_peaks, baselin
     err_peak = np.sqrt(peak)
 
     g2 = cent / peak
-    err_g2 = g2 * np.sqrt((err_cent / cent) ** 2 + (err_peak / peak) ** 2)
 
-    return g2, err_g2
+    return g2
 
 
 def get_HOM_1input(dat_HOM, peak_width, peak_sep, central_peak, num_peaks, baseline=True):
@@ -123,9 +123,8 @@ def get_HOM_1input(dat_HOM, peak_width, peak_sep, central_peak, num_peaks, basel
     err_peak = np.sqrt(peak)
 
     V = 1 - 2 * cent / peak
-    err_V = (1 - V) * np.sqrt((err_cent / cent) ** 2 + (err_peak / peak) ** 2)
 
-    return V, err_V
+    return V
 
 
 def get_HOM_2input(HOM_ortho, HOM_para, num_peaks=6, baseline=True, plotit=False, manualmode=False,

fit_g2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 @Authors: Mathias Pont
-@Contributors:
+@Contributors: Andreas Fyrillas
 
 This script gets the g2(0) of an histogram.
 Input: .txt (Swabian) or .dat (HydraHarp) file downloaded from your computer using the app
@@ -89,9 +89,20 @@ def main():
         peak_sep = st.sidebar.number_input('Peak separation', 0, len(data), pk_sep)
         base_line = st.sidebar.checkbox('Substract baseline', value=True)
 
-        g2, errg2 = get_g2_1input(data, peak_width, peak_sep, central_peak, num_peaks, baseline=base_line)
+        # Compute g2
+        g2 = get_g2_1input(data, peak_width, peak_sep, central_peak, num_peaks, baseline=base_line)
+        # Compute error on g2
+        errg2 = np.std([get_g2_1input(np.random.poisson(data),
+                                      peak_width,
+                                      peak_sep,
+                                      central_peak,
+                                      num_peaks,
+                                      baseline=base_line)
+                        for sim in range(100)
+                        ]
+                       )
 
-        title_fig = 'g2 =' + str(round(g2, 4)) + '±' + str(round(errg2, 4))
+        title_fig = f'g2 = {g2 * 100:.3} \u00B1 {errg2 * 100:.2} %'
 
         # Show integrations windows
         show_details = st.sidebar.checkbox('Show details', value=True)